a3e74217c8ed7232cda9061cdc8ae28287881ab0
Franziska Hinkelmann <hinkelmann.1@mbi.osu.edu>
2013-07-02 22:20:15 -0400

    port can be read from command line

--- Maintenance.js
+++ Maintenance.js
@@ -2,10 +2,15 @@ var http = require('http');
 
 var server = http.createServer();
 server.on('request', function(request, response) {
-    response.writeHead('200', {'content-type' : 'plain/text'});
+    response.writeHead('200', {'content-type' : 'text/html'});
     response.write('Polynome is currently unavailable as we undergo server maintenance. We apologize for the inconvenience.');
     response.end();
 });
 
-server.listen(3002);
-console.log("Maintenance message running at 3002.")
+
+var port = 3002;
+if (process.argv[2]) {
+    port = parseInt(process.argv[2], 10) ;
+}
+server.listen(port);
+console.log("Maintenance message running at " + port + ".")
